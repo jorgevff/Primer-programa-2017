ed1429379b2f93509860b9b7d8f2636b3174ca01
jorgevff <jvalladaresfontano@gmail.com>
2017-09-22 13:17:32 +0200

    Muestra la localidad

--- nombre.java
+++ nombre.java
@@ -2,6 +2,7 @@ public class nombre{
 	public static void main(String args[]){
 		System.out.println("Mi nombre es Jorge Valladares!");
 		System.out.println("Tengo 46 años");
+		System.out.println("Vivo en Villarrodrigo de las Regueras");
 	}
 
 }
