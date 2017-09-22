67225ffdc668f906ecca13a1756b46464e1012a4
jorgevff <jvalladaresfontano@gmail.com>
2017-09-22 13:25:23 +0200

    Muestra mes de creacion

--- nombre.java
+++ nombre.java
@@ -3,6 +3,7 @@ public class nombre{
 		System.out.println("Mi nombre es Jorge Valladares!");
 		System.out.println("Tengo 46 años");
 		System.out.println("Vivo en Villarrodrigo de las Regueras");
+		System.out.println("Mes de Septiembre");
 	}
 
 }
